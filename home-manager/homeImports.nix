39e425164636c5ae25d5c4954b31cde67eb36ed7
Cronos6096 <and.merciaro@gmail.com>
2025-04-27 20:56:17 +0200

    Refactoring generale

--- home-manager/homeImports.nix
+++ home-manager/homeImports.nix
@@ -1,11 +1,5 @@
 {
   imports = [
-    ../moduli/Git.nix
-    ../moduli/Hyprland.nix
-    ../moduli/Nixvim/default.nix
-    ../moduli/Terminale.nix
-    ../moduli/waybar
-    ../moduli/Walker.nix
-    # ../moduli/Stylix.nix
+    ../moduli/home-manager
   ];
 }
---
+++ moduli/home-manager/default.nix
@@ -0,0 +1,11 @@
+{
+  imports = [
+    ../moduli/Git.nix
+    ../moduli/Hyprland.nix
+    ../moduli/Nixvim
+    ../moduli/Terminale.nix
+    ../moduli/Walker.nix
+    ../moduli/waybar
+    # ../moduli/Stylix.nix
+  ];
+}
