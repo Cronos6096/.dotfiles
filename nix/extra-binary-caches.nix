b03b9383ea5e3af05a5e6b8e051c3e6a01bcaaa1
Cronos6096 <and.merciaro@gmail.com>
2025-10-11 13:32:31 +0200

    Riaggiunte cache di walker

--- nix/extra-binary-caches.nix
+++ nix/extra-binary-caches.nix
@@ -10,13 +10,13 @@
 
     builders-use-substitutes = true;
     extra-substituters = [
-      # "https://walker.cachix.org"
+      "https://walker.cachix.org"
       "https://cache.garnix.io"
       "https://cache.nixos.org https://nixos-raspberrypi.cachix.org"
     ];
 
     extra-trusted-public-keys = [
-      # "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
       "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI="
     ];
