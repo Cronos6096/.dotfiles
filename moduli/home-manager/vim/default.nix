a1513f0160b15fa6b9c66fbc296dc4474c1f9c03
Cronos6096 <and.merciaro@gmail.com>
2025-10-21 16:08:08 +0200

    Rimossa vecchia configurazione di vim

--- moduli/home-manager/vim/default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    neovim
-  ];
-
-  # xdg.configFile."nvim".source = ./nvim;
-}
--- moduli/home-manager/vim/nvim/lua/config/binds.lua
+++
@@ -1,2 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>z", vim.lsp.buf.format)
