153401643a048336d32d4dbd5aaefea4dae7a227
Cronos6096 <and.merciaro@gmail.com>
2025-12-13 12:25:59 +0100

    Portali xdg che vanno

--- moduli/system/Portals.nix
+++ moduli/system/Portals.nix
@@ -3,23 +3,26 @@
   xdg = {
     portal = {
       enable = true;
-      xdgOpenUsePortal = true;
+       xdgOpenUsePortal = false;
       extraPortals = with pkgs; [
         xdg-desktop-portal-hyprland
-        kdePackages.xdg-desktop-portal-kde
+        xdg-desktop-portal-gtk
       ];
-      config = {
-        common = {
-          default = [ "kde" ];
-          "org.freedesktop.impl.portal.FileChooser" = "kde";
+      config =
+        let
+          hyprlandPortalPreferences = {
+            default = [ "hyprland" "gtk" ];
+            "org.freedesktop.impl.portal.Screenshot" = "hyprland";
+            "org.freedesktop.impl.portal.Screencast" = "hyprland";
+            "org.freedesktop.impl.portal.FileChooser" = "gtk";
+            "org.freedesktop.impl.portal.OpenURI" = "gtk";
+          };
+        in
+        {
+          common = hyprlandPortalPreferences;
+          Hyprland = hyprlandPortalPreferences;
+          hyprland = hyprlandPortalPreferences;
         };
-        hyprland = {
-          default = [ "hyprland" "kde" ];
-          "org.freedesktop.impl.portal.Screenshot" = "hyprland";
-          "org.freedesktop.impl.portal.Screencast" = "hyprland";
-          "org.freedesktop.impl.portal.FileChooser" = "kde";
-        };
-      };
     };
 
     menus.enable = true;
