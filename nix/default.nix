bc2519e0daa4285b602d82dbd097e7f55b2bafdb
Cronos6096 <and.merciaro@gmail.com>
2026-01-06 09:34:28 +0100

    Arm crosscompile

--- nix/default.nix
+++ nix/default.nix
@@ -1,8 +1,23 @@
 {
   imports = [ ./extra-binary-caches.nix ];
-  nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+
+  nixpkgs.config = {
+    allowUnfree = true;
+  };
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    extra-platforms = [ "aarch64-linux" ];
+
+    trusted-users = [
+      "root"
+      "@wheel"
+    ];
+  };
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
 }
