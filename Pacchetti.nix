58c7a83410d02d583534b29a1a28130605002d9c
Cronos6096 <and.merciaro@gmail.com>
2026-04-07 09:54:56 +0200

    Kicad fixato

--- Pacchetti.nix
+++ Pacchetti.nix
@@ -3,6 +3,28 @@
   ...
 }:
 
+let
+  kicadLibs = pkgs.kicad.passthru.libraries;
+  kicadTemplateDir = pkgs.symlinkJoin {
+    name = "KiCad_template_dir";
+    paths = with kicadLibs; [
+      "${templates}/share/kicad/template"
+      "${footprints}/share/kicad/template"
+      "${symbols}/share/kicad/template"
+    ];
+  };
+
+  kicadWithUpdatedEnv = pkgs.kicad.overrideAttrs (oldAttrs: {
+    makeWrapperArgs = oldAttrs.makeWrapperArgs ++ [
+      "--set-default KICAD10_SYMBOL_DIR ${kicadLibs.symbols}/share/kicad/symbols"
+      "--set-default KICAD10_FOOTPRINT_DIR ${kicadLibs.footprints}/share/kicad/footprints"
+      "--set-default KICAD10_3DMODEL_DIR ${kicadLibs.packages3d}/share/kicad/3dmodels"
+      "--set-default KICAD10_TEMPLATE_DIR ${kicadTemplateDir}"
+    ];
+  });
+
+in
+
 {
   environment.systemPackages = with pkgs; [
     # Codice
@@ -55,7 +77,7 @@
     zathura
 
     qalculate-gtk
-    kicad
+    kicadWithUpdatedEnv
 
     # Giochi e compatibilità Windows
     wineWow64Packages.full
