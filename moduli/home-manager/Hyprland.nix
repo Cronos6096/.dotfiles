aab1c905a843004ef8d8a8fded0e786fda6a79a0
Cronos6096 <and.merciaro@gmail.com>
2026-05-01 15:39:02 +0200

    Rimosse dependency inutili

--- moduli/home-manager/Hyprland.nix
+++ moduli/home-manager/Hyprland.nix
@@ -14,18 +14,17 @@ with lib;
     };
 
     home = {
-      file."/home/andme/.config/hypr/hyprland.conf".source =
-        config.lib.file.mkOutOfStoreSymlink ../../moduli/wm/hypr/hyprland.conf;
+      file."/home/andme/.config/hypr/hyprland.conf".source = mkForce (
+        config.lib.file.mkOutOfStoreSymlink "/home/andme/.dotfiles/moduli/wm/hypr/hyprland.conf"
+      );
       file."/home/andme/.config/pypr/config.toml".source =
-        config.lib.file.mkOutOfStoreSymlink ../../moduli/wm/hypr/pyprland.toml;
+        config.lib.file.mkOutOfStoreSymlink "/home/andme/.dotfiles/moduli/wm/hypr/pyprland.toml";
 
       packages = with pkgs; [
         wl-clipboard
         clipse
         hyprshot
-        pavucontrol
         networkmanager
-        handlr
         hyprsome
         pyprland
         jq
