b38b05883d5241f58b8507a6d2bd3279ec4656d1
Cronos6096 <and.merciaro@gmail.com>
2026-05-10 09:29:40 +0200

    Immich e nginx proxy manager

--- sys/rpi/server.nix
+++ sys/rpi/server.nix
@@ -61,10 +61,38 @@
           "--cap-add=SYS_ADMIN"
         ];
       };
+
+      nginx-proxy-manager = {
+        image = "jc21/nginx-proxy-manager:latest";
+        autoStart = true;
+        ports = [
+          "80:80"
+          "81:81"
+          "443:443"
+        ];
+
+        environment = {
+          TZ = "Europe/Rome";
+        };
+
+        volumes = [
+          "/var/lib/nginx-proxy-manager/data:/data"
+          "/var/lib/nginx-proxy-manager/letsencrypt:/etc/letsencrypt"
+        ];
+      };
     };
   };
 
   services = {
+    immich = {
+      enable = true;
+      port = 2283;
+      host = "0.0.0.0";
+      openFirewall = true;
+      environment.IMMICH_LOG_LEVEL = "warn";
+      mediaLocation = "/var/lib/immich";
+    };
+
     duckdns = {
       enable = true;
       tokenFile = config.age.secrets.duckdnsToken.path;
