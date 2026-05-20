3ad98e9a9b2d41e9c94567ebcb202a00c7e88be8
Cronos6096 <and.merciaro@gmail.com>
2026-05-20 19:18:54 +0200

    Symlink funzionanti

--- moduli/home-manager/Hyprland.nix
+++ moduli/home-manager/Hyprland.nix
@@ -11,26 +11,28 @@ with lib;
       enable = true;
       package = null;
       portalPackage = null;
+      configType = "hyprlang";
     };
     home = {
       file = {
-        "/home/andme/.config/hypr/autostart.lua".source = mkForce (
-          config.lib.file.mkOutOfStoreSymlink "../wm/hypr/autostart.lua"
+        ".config/hypr/autostart.lua".source = mkForce (
+          config.lib.file.mkOutOfStoreSymlink ./../wm/hypr/autostart.lua
         );
-        "/home/andme/.config/hypr/env.lua".source = mkForce (
-          config.lib.file.mkOutOfStoreSymlink "../wm/hypr/env.lua"
+        ".config/hypr/env.lua".source = mkForce (config.lib.file.mkOutOfStoreSymlink ./../wm/hypr/env.lua);
+        ".config/hypr/hyprland.lua".source = mkForce (
+          config.lib.file.mkOutOfStoreSymlink ./../wm/hypr/hyprland.lua
         );
-        "/home/andme/.config/hypr/hyprland.lua".source = mkForce (
-          config.lib.file.mkOutOfStoreSymlink "../wm/hypr/hyprland.lua"
+        ".config/hypr/looks.lua".source = mkForce (
+          config.lib.file.mkOutOfStoreSymlink ./../wm/hypr/looks.lua
         );
-        "/home/andme/.config/hypr/looks.lua".source = mkForce (
-          config.lib.file.mkOutOfStoreSymlink "../wm/hypr/looks.lua"
+        ".config/hypr/keybinds.lua".source = mkForce (
+          config.lib.file.mkOutOfStoreSymlink ./../wm/hypr/keybinds.lua
         );
-        "/home/andme/.config/hypr/monitors.lua".source = mkForce (
-          config.lib.file.mkOutOfStoreSymlink "../wm/hypr/monitors.lua"
+        ".config/hypr/monitors.lua".source = mkForce (
+          config.lib.file.mkOutOfStoreSymlink ./../wm/hypr/monitors.lua
         );
-        "/home/andme/.config/pypr/config.toml".source = mkForce (
-          config.lib.file.mkOutOfStoreSymlink "..wm/hypr/pyprland.toml"
+        ".config/pypr/config.toml".source = mkForce (
+          config.lib.file.mkOutOfStoreSymlink ./../wm/hypr/pyprland.toml
         );
       };
       packages = with pkgs; [
