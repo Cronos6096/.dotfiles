193c8a02130daa0a508136027fdce89ffb51eb48
Cronos6096 <and.merciaro@gmail.com>
2026-06-29 11:53:51 +0200

    Rimosso garnix

--- nix/extra-binary-caches.nix
+++ nix/extra-binary-caches.nix
@@ -5,7 +5,6 @@
     builders-use-substitutes = true;
     extra-substituters = [
       "https://attic.xuyh0120.win/lantian"
-      "https://cache.garnix.io"
       "https://cache.nixos.org"
       "https://cuda-maintainers.cachix.org"
       "https://hyprland.cachix.org"
@@ -15,7 +14,6 @@
     ];
 
     extra-trusted-public-keys = [
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
